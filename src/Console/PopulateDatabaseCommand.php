7a0e6803fedfac95161be9a303604c919ae29359
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-01-21 22:53:05 +0100

    faker db

--- src/Console/PopulateDatabaseCommand.php
+++ src/Console/PopulateDatabaseCommand.php
@@ -10,6 +10,7 @@ use Slim\App;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Faker;
 
 class PopulateDatabaseCommand extends Command
 {
@@ -40,29 +41,66 @@ class PopulateDatabaseCommand extends Command
         $db->getConnection()->statement("TRUNCATE `companies`");
         $db->getConnection()->statement("SET FOREIGN_KEY_CHECKS=1");
 
-
+        /**
         $db->getConnection()->statement("INSERT INTO `companies` VALUES
-    (1,'Stack Exchange','0601010101','stack@exchange.com','https://stackexchange.com/','https://i.stack.imgur.com/UPdHB.jpg', now(), now(), null),
-    (2,'Google','0602020202','contact@google.com','https://www.google.com','https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Google_office_%284135991953%29.jpg/800px-Google_office_%284135991953%29.jpg?20190722090506',now(), now(), null)
+        (1,'Stack Exchange','0601010101','stack@exchange.com','https://stackexchange.com/','https://i.stack.imgur.com/UPdHB.jpg', now(), now(), null),
+        (2,'Google','0602020202','contact@google.com','https://www.google.com','https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Google_office_%284135991953%29.jpg/800px-Google_office_%284135991953%29.jpg?20190722090506',now(), now(), null)
         ");
 
         $db->getConnection()->statement("INSERT INTO `offices` VALUES
-    (1,'Bureau de Nancy','1 rue Stanistlas','Nancy','54000','France','nancy@stackexchange.com',NULL,1, now(), now()),
-    (2,'Burea de Vandoeuvre','46 avenue Jeanne d\'Arc','Vandoeuvre','54500','France',NULL,NULL,1, now(), now()),
-    (3,'Siege sociale','2 rue de la primatiale','Paris','75000','France',NULL,NULL,2, now(), now()),
-    (4,'Bureau Berlinois','192 avenue central','Berlin','12277','Allemagne',NULL,NULL,2, now(), now())
+        (1,'Bureau de Nancy','1 rue Stanistlas','Nancy','54000','France','nancy@stackexchange.com',NULL,1, now(), now()),
+        (2,'Burea de Vandoeuvre','46 avenue Jeanne d\'Arc','Vandoeuvre','54500','France',NULL,NULL,1, now(), now()),
+        (3,'Siege sociale','2 rue de la primatiale','Paris','75000','France',NULL,NULL,2, now(), now()),
+        (4,'Bureau Berlinois','192 avenue central','Berlin','12277','Allemagne',NULL,NULL,2, now(), now())
         ");
 
         $db->getConnection()->statement("INSERT INTO `employees` VALUES
-     (1,'Camille','La Chenille',1,'camille.la@chenille.com',NULL,'Ingénieur', now(), now()),
-     (2,'Albert','Mudhat',2,'albert.mudhat@aqume.net',NULL,'Superviseur', now(), now()),
-     (3,'Sylvie','Tesse',3,'sylive.tesse@factice.local',NULL,'PDG', now(), now()),
-     (4,'John','Doe',4,'john.doe@generique.org',NULL,'Testeur', now(), now()),
-     (5,'Jean','Bon',1,'jean@test.com',NULL,'Developpeur', now(), now()),
-     (6,'Anais','Dufour',2,'anais@aqume.net',NULL,'DBA', now(), now()),
-     (7,'Sylvain','Poirson',3,'sylvain@factice.local',NULL,'Administrateur réseau', now(), now()),
-     (8,'Telma','Thiriet',4,'telma@generique.org',NULL,'Juriste', now(), now())
-        ");
+        (1,'Camille','La Chenille',1,'camille.la@chenille.com',NULL,'Ingénieur', now(), now()),
+        (2,'Albert','Mudhat',2,'albert.mudhat@aqume.net',NULL,'Superviseur', now(), now()),
+        (3,'Sylvie','Tesse',3,'sylive.tesse@factice.local',NULL,'PDG', now(), now()),
+        (4,'John','Doe',4,'john.doe@generique.org',NULL,'Testeur', now(), now()),
+        (5,'Jean','Bon',1,'jean@test.com',NULL,'Developpeur', now(), now()),
+        (6,'Anais','Dufour',2,'anais@aqume.net',NULL,'DBA', now(), now()),
+        (7,'Sylvain','Poirson',3,'sylvain@factice.local',NULL,'Administrateur réseau', now(), now()),
+        (8,'Telma','Thiriet',4,'telma@generique.org',NULL,'Juriste', now(), now())
+        ");**/
+
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 3; $i++) {
+            $company = new Company();
+            $company->name = $faker->company();
+            $company->phone = $faker->phoneNumber();
+            $company->email = $faker->email();
+            $company->website = $faker->url();
+            $company->image = $faker->imageUrl();
+            $company->save();
+
+            for ($j = 0; $j < 3; $j++) {
+                $office = new Office();
+                $office->name = $faker->company();
+                $office->address = $faker->streetAddress();
+                $office->city = $faker->city();
+                $office->zip_code = $faker->postcode();
+                $office->country = $faker->country();
+                $office->email = $faker->email();
+                $office->phone = $faker->phoneNumber();
+                $office->company_id = $company->id;
+                $office->save();
+
+                for ($k = 0; $k < 3; $k++) {
+                    $employee = new Employee();
+                    $employee->first_name = $faker->firstName();
+                    $employee->last_name = $faker->lastName();
+                    $employee->office_id = $office->id;
+                    $employee->email = $faker->email();
+                    $employee->phone = $faker->phoneNumber();
+                    $employee->job_title = $faker->jobTitle();
+                    $employee->save();
+                }
+
+            }
+        }
 
         $db->getConnection()->statement("update companies set head_office_id = 1 where id = 1;");
         $db->getConnection()->statement("update companies set head_office_id = 3 where id = 2;");
